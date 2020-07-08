de4f7ee95ef197787f0fa6df753a45f73653ae79
LAPTOP-3UFQ56MN\monts <montse@montse.com>
2020-07-08 17:51:57 +0200

    FASE I txt Fecha

---
+++ Fecha.java
@@ -0,0 +1,10 @@
+public class Fecha {
+    public static void main(String [] args) {
+
+	//Declaro variables Int
+    int= dia= 8;
+    int= mes= "Julio";
+    int= año = 2020;
+    	System.out.println ("+dia+"/"+mes+"/"+año+");
+   }
+}
