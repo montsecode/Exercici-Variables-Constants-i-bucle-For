e58a7697e426ab584c870bbfe51436614bc1bbb0
LAPTOP-3UFQ56MN\monts <montse@montse.com>
2020-07-08 17:33:55 +0200

    FASE I txt Nombre y apellidos

---
+++ NombreyApellidos.java
@@ -0,0 +1,10 @@
+public class NombreyApellidos {
+    public static void main(String [] args) {
+
+	//Declaro variables String
+    nombre= "Montse";
+    apellido1 = "González";
+    apellido2 = "Puentes";
+    	System.out.println (.concat(apellido1).concat(apellido2).concat(nombre);
+   }
+}
