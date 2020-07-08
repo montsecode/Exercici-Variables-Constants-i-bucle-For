c78ef9077a95eb194ddbc0eb92745f0353c868df
LAPTOP-3UFQ56MN\monts <montse@montse.com>
2020-07-08 20:26:21 +0200

    Modif FASE I AP 2

--- Fecha.java
+++ Fecha.java
@@ -1,10 +1,11 @@
 public class Fecha {
+  
     public static void main(String [] args) {
 
 	//Declaro variables Int
     int dia = 8;
     int mes = 7;
-    int año = 2020;
-    	System.out.println ("+dia+"/"+mes+"/"+año+");
+    int anio = 2020;
+    	System.out.println ("+dia+"/"+mes+"/"+anio+");
    }
 }
