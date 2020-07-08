8e086b8b080531e74b545eb75a0a6f0cf7019348
LAPTOP-3UFQ56MN\monts <montse@montse.com>
2020-07-08 18:30:38 +0200

    modif

--- NombreyApellidos.java
+++ NombreyApellidos.java
@@ -2,9 +2,10 @@ public class NombreyApellidos {
     public static void main(String [] args) {
 
 	//Declaro variables String
-    nombre= "Montse";
-    apellido1 = "González";
-    apellido2 = "Puentes";
-    	System.out.println (.concat(apellido1).concat(apellido2).concat(nombre);
+    String nombre= "Montse";
+    String apellido1 = "González";
+    String apellido2 = "Puentes";
+    	System.out.println (.concat(apellido1).concat(apellido2).concat(nombre));
    }
 }
+
