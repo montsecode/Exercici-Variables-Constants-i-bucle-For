80d9fef33a26b754b8bc2e74b5fdea86f72cd3bc
LAPTOP-3UFQ56MN\monts <montse@montse.com>
2020-09-29 19:45:11 +0200

    updated

--- ExerciciVariablesConstantsBucleFor/src/Fase2.java
+++ ExerciciVariablesConstantsBucleFor/src/Fase2.java
@@ -1,11 +1,11 @@
 /**
  *FASE 2
 Sabent que l’any 1948 es un any de traspàs:
-● Creeu una constant amb el valor de l’any (1948).
-● Creeu una variable que guardi cada quan hi ha un any de traspàs.
-● Calculeu quants anys de traspàs hi ha entre 1948 i el vostre any de naixement i emmagatzemeu el valor
+* Creeu una constant amb el valor de l’any (1948).
+* Creeu una variable que guardi cada quan hi ha un any de traspàs.
+* Calculeu quants anys de traspàs hi ha entre 1948 i el vostre any de naixement i emmagatzemeu el valor
 resultant en una variable.
-● Mostreu per pantalla el resultat del càlcul
+* Mostreu per pantalla el resultat del càlcul
 
  * @author monts
  *
@@ -14,35 +14,38 @@ resultant en una variable.
 import java.util.Scanner;
 public class Fase2 {
 	
-	   final int anio = 1948;
+	final int anio = 1948;
 
 	public static void main(String[] args) {
 	
 		//Los años son guardadas como variables
 		int anio = 1948;
 		int nacimientoanio = 1975;
-		//Comprobamos si el primer año introducido es bisiesto y mostramos los años
+		//Comprobamos si el primer año  es bisiesto y mostramos los años
 		if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))) {
             for (int contador = anio; contador = nacimientoanio ; contador += 4) {
                 System.out.println(contador);
 			
             }
-        //si no es bisiesto, entonces tenemos que trabajar de la siguiente forma
+            
+            
+        //si no es bisiesto creamos  variable temporal para comparación en el bucle
 		}else {
-		//Creamos una variable temporal para realizar la comparación en el bucle. 
-        // La inicializamos con el año de la primera fecha
-		int aniob = anio
+		 
+        // La inicializamos la variable temporal con el año de la primera fecha
+		int aniob = anio;
+		
 		//En cada paso por el bucle, aumentamos en uno la variable temporal. 
 		//La condición para seguir en el bucle es que el año de la variable 
 		 //temporal sea igual o inferior al añonacimiento
 			do {
 				aniob = aniob + 1;
-			}while ((aniob % 4 != 0) && ((aniob % 100 == 0) || (aniob % 400 != 0)));
+			} while ((aniob % 4 != 0) && ((aniob % 100 == 0) || (aniob % 400 != 0)));
 			
 			
 			//Con un bucle for, imprimimos los años: comenzamos con la variable 
-            // temporal, aumentamos 4 años y terminamos con el año de la fecha 2
-			for (int contador = anio; contador = nacimientoanio; contador += 4) {		
+            // temporal, aumentamos 4 años y terminamos con el año de la fecha nacimiento
+			for (int contador = aniob; contador = nacimientoanio; contador += 4) {		
 				System.out.println(contador);
 					
 	
